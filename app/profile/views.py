e0d65b682a01a4c0ac62324f3aa9fa80c4416f11
brianmwevi <mwevibrian@gmail.com>
2022-05-27 13:26:58 +0300

    ft:update -- add view to update user profile image

--- app/profile/views.py
+++ app/profile/views.py
@@ -13,3 +13,21 @@ def profile(user_id):
     articles = Article.query.order_by(
         desc('created_at')).filter_by(author_id=user_id).all()
     return render_template('profile.html', articles=articles)
+
+
+@user.route('/profile/<int:user_id>/update', methods=['POST'])
+def update(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if 'photo' in request.files:
+        if user.profile_pic_path:
+            user_image = f"/{user.profile_pic_path.split('/')[1]}"
+            path = os.path.abspath(os.environ.get(
+                'UPLOADED_PHOTOS_DEST') + user_image)
+            if os.path.exists(path):
+                os.remove(path)
+
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        user.save()
+    return redirect(url_for('user.profile', user_id=user_id))
