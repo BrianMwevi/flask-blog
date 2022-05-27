606633c02fbd3bb320d6823e730e534e3787f0ff
brianmwevi <mwevibrian@gmail.com>
2022-05-27 13:33:52 +0300

    ft:new comment -- add new comment creation view

---
+++ app/comment/views.py
@@ -0,0 +1,20 @@
+from flask import redirect, url_for, flash
+from app.comment.forms import CommentForm
+from app.models import Comment
+from app.urls import comment
+from flask_login import current_user, login_required
+
+
+# New Comment View
+@login_required
+@comment.route('new/<int:article_id>', methods=['POST'])
+def new_comment(article_id):
+    comment_form = CommentForm()
+    if comment_form.validate_on_submit():
+        comment = Comment(body=comment_form.body.data,
+                          user_id=current_user.id, article_id=article_id)
+        comment.save()
+        flash("Comment created successfully!")
+    else:
+        flash("Please fill the form with the correct details and try again")
+    return redirect(url_for('article.detail', article_id=article_id))
